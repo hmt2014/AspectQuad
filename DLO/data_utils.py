6b42f094854b2bb63915d092e81082555e148d09
hmt2014 <humengting_tongji@163.com>
2022-11-09 20:40:13 +0800

    codes

---
+++ DLO/data_utils.py
@@ -0,0 +1,389 @@
+# -*- coding: utf-8 -*-
+
+# This script contains all data transformation and reading
+
+import random
+from torch.utils.data import Dataset
+from itertools import permutations
+from transformers import AdamW, T5Tokenizer, T5ForConditionalGeneration
+from t5_score import MyT5ForConditionalGenerationScore
+from itertools import permutations
+import torch
+import numpy as np
+
+senttag2word = {'POS': 'positive', 'NEG': 'negative', 'NEU': 'neutral'}
+senttag2opinion = {'POS': 'great', 'NEG': 'bad', 'NEU': 'ok'}
+sentword2opinion = {'positive': 'great', 'negative': 'bad', 'neutral': 'ok'}
+
+aspect_cate_list = ['location general',
+                    'food prices',
+                    'food quality',
+                    'food general',
+                    'ambience general',
+                    'service general',
+                    'restaurant prices',
+                    'drinks prices',
+                    'restaurant miscellaneous',
+                    'drinks quality',
+                    'drinks style_options',
+                    'restaurant general',
+                    'food style_options']
+
+
+def read_line_examples_from_file(data_path, silence=True):
+    """
+    Read data from file, each line is: sent####labels
+    Return List[List[word]], List[Tuple]
+    """
+    sents, labels = [], []
+    with open(data_path, 'r', encoding='UTF-8') as fp:
+        words, labels = [], []
+        for line in fp:
+            line = line.strip()
+            if line != '':
+                words, tuples = line.split('####')
+                sents.append(words.split())
+                labels.append(eval(tuples))
+    if silence:
+        print(f"Total examples = {len(sents)}")
+    return sents, labels
+
+
+def get_para_aste_targets(sents, labels):
+    targets = []
+    for i, label in enumerate(labels):
+        all_tri_sentences = []
+        for tri in label:
+            # a is an aspect term
+            if len(tri[0]) == 1:
+                a = sents[i][tri[0][0]]
+            else:
+                start_idx, end_idx = tri[0][0], tri[0][-1]
+                a = ' '.join(sents[i][start_idx:end_idx+1])
+
+            # b is an opinion term
+            if len(tri[1]) == 1:
+                b = sents[i][tri[1][0]]
+            else:
+                start_idx, end_idx = tri[1][0], tri[1][-1]
+                b = ' '.join(sents[i][start_idx:end_idx+1])
+
+            # c is the sentiment polarity
+            c = senttag2opinion[tri[2]]           # 'POS' -> 'good'
+
+            one_tri = f"It is {c} because {a} is {b}"
+            all_tri_sentences.append(one_tri)
+        targets.append(' [SSEP] '.join(all_tri_sentences))
+    return targets
+
+
+def get_para_tasd_targets(sents, labels):
+
+    targets = []
+    for label in labels:
+        all_tri_sentences = []
+        for triplet in label:
+            at, ac, sp = triplet
+
+            man_ot = sentword2opinion[sp]   # 'positive' -> 'great'
+
+            if at == 'NULL':
+                at = 'it'
+            one_tri = f"{ac} is {man_ot} because {at} is {man_ot}"
+            all_tri_sentences.append(one_tri)
+
+        target = ' [SSEP] '.join(all_tri_sentences)
+        targets.append(target)
+    return targets
+
+def order_scores_function(quad_list, cur_sent, model, tokenizer, device):
+    q = ["[AT]", "[OT]", "[AC]", "[SP]"]
+    all_orders = permutations(q)
+    all_orders_list = []
+
+    all_targets = []
+    all_inputs = []
+    cur_sent = " ".join(cur_sent)
+    for each_order in all_orders:
+        cur_order = " ".join(each_order)
+        all_orders_list.append(cur_order)
+        cur_target = []
+        for each_q in quad_list:
+            cur_target.append(each_q[cur_order][0])
+
+        all_inputs.append(cur_sent)
+        all_targets.append(" ".join(cur_target))
+
+    tokenized_input = tokenizer.batch_encode_plus(
+        all_inputs, max_length=200, padding="max_length",
+        truncation=True, return_tensors="pt"
+    )
+    tokenized_target = tokenizer.batch_encode_plus(
+        all_targets, max_length=200, padding="max_length",
+        truncation=True, return_tensors="pt"
+    )
+
+    target_ids = tokenized_target["input_ids"].to(device)
+
+    target_ids[target_ids[:, :] == tokenizer.pad_token_id] = -100
+    outputs = model(
+        input_ids=tokenized_input["input_ids"].to(device),
+        attention_mask=tokenized_input["attention_mask"].to(device),
+        labels=target_ids,
+        decoder_attention_mask=tokenized_target["attention_mask"].to(device)
+    )
+
+    loss, entropy = outputs[0]
+    results = {}
+    for i, _ in enumerate(all_orders_list):
+        cur_order = all_orders_list[i]
+        results[cur_order] = {"loss": loss[i], "entropy": entropy[i]}
+    #print(best_quad)
+    return results
+
+def choose_best_order_global(sents, labels, model, tokenizer, device, top_k):
+    q = ["[AT]", "[OT]", "[AC]", "[SP]"]
+    all_orders = permutations(q)
+    all_orders_list = []
+    scores = []
+
+    for each_order in all_orders:
+        cur_order = " ".join(each_order)
+        all_orders_list.append(cur_order)
+        scores.append(0)
+
+    for i in range(len(sents)):
+        label = labels[i]
+        cur_sent = sents[i]
+
+
+        quad_list = []
+        for quad in label:
+            at, ac, sp, ot = quad
+
+            man_ot = sentword2opinion[sp]  # 'POS' -> 'good'
+
+            if at == 'NULL':  # for implicit aspect term
+                at = 'it'
+
+            quad = [f"[AT] {at}",
+                    f"[OT] {ot}",
+                    f"[AC] {ac}",
+                    f"[SP] {man_ot}"]
+            x = permutations(quad)
+
+            permute_object = {}
+            for each in x:
+                order = []
+                content = []
+                for e in each:
+                    order.append(e[0:4])
+                    content.append(e[4:])
+                order_name = " ".join(order)
+                content = " ".join(content)
+                permute_object[order_name] = [content, " ".join(each)]
+
+            quad_list.append(permute_object)
+
+        order_scores = order_scores_function(quad_list, cur_sent, model, tokenizer, device)
+        for e in order_scores:
+            index = all_orders_list.index(e)
+            scores[index] += order_scores[e]['entropy']
+
+
+    ###### !!!!!!!! IMPORTANT. control entropy min, entropy max, random
+    """ # random
+    indexes = list(range(len(scores)))
+    random.shuffle(indexes)
+    indexes = indexes[0:top_k]
+    """
+    indexes = np.argsort(np.array(scores))[0:top_k]#[::-1] #
+    #print(scores)
+    returned_orders = []
+    for i in indexes:
+        returned_orders.append(all_orders_list[i])
+    return returned_orders
+
+def get_para_asqp_targets(sents, labels, top_k):
+    """
+    Obtain the target sentence under the paraphrase paradigm
+    """
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+    else:
+        device = torch.device("cpu")
+    tokenizer = T5Tokenizer.from_pretrained("t5-base")#.to(device)
+    #model_org = T5ForConditionalGeneration.from_pretrained("t5-base")
+    #torch.save(model_org, "save.pt")
+    #model = MyT5ForConditionalGenerationScore.load_state_dict(state_dict=torch.load("save.pt"), strict=True).to(device)
+    model = MyT5ForConditionalGenerationScore.from_pretrained("t5-base").to(device)
+    targets = []
+    new_sents = []
+    data_count = {}
+    max_length_input = 0
+    max_length_target = 0
+
+    optim_orders = choose_best_order_global(sents, labels, model, tokenizer, device, top_k)
+
+    for i in range(len(sents)):
+        label = labels[i]
+        cur_sent = sents[i]
+
+        if len(label) in data_count:
+            data_count[len(label)] += 1
+        else:
+            data_count[len(label)] = 1
+
+        all_quad_sentences = []
+
+        already_output = ""
+
+        quad_list = []
+        for quad in label:
+            at, ac, sp, ot = quad
+
+            man_ot = sentword2opinion[sp]  # 'POS' -> 'good'
+
+            if at == 'NULL':  # for implicit aspect term
+                at = 'it'
+
+            quad = [f"[AT] {at}",
+                    f"[OT] {ot}",
+                    f"[AC] {ac}",
+                    f"[SP] {man_ot}"]
+            x = permutations(quad)
+            permute_object = {}
+            for each in x:
+                order = []
+                content = []
+                for e in each:
+                    order.append(e[0:4])
+                    content.append(e[4:])
+                order_name = " ".join(order)
+                content = " ".join(content)
+                permute_object[order_name] = [content, " ".join(each)]
+
+            quad_list.append(permute_object)
+
+        for o in optim_orders:
+            tar = []
+            for each_q in quad_list:
+                tar.append(each_q[o][1])
+
+            targets.append(" [SSEP] ".join(tar))
+            new_sents.append(cur_sent)
+
+    return new_sents, targets
+
+def get_para_asqp_targets_test(sents, labels):
+    """
+    Obtain the target sentence under the paraphrase paradigm
+    """
+    targets = []
+    for label in labels:
+        all_quad_sentences = []
+        for quad in label:
+            at, ac, sp, ot = quad
+
+            man_ot = sentword2opinion[sp]  # 'POS' -> 'good'
+
+            if at == 'NULL':  # for implicit aspect term
+                at = 'it'
+
+            quad_list = [f"[AT] {at}", f"[OT] {ot}", f"[AC] {ac}", f"[SP] {man_ot}"]
+            one_quad_sentence = " ".join(quad_list)
+            all_quad_sentences.append(one_quad_sentence)
+
+        target = ' [SSEP] '.join(all_quad_sentences)
+        targets.append(target)
+    return targets
+
+def get_transformed_io(data_path, data_dir, data_type, top_k):
+    """
+    The main function to transform input & target according to the task
+    """
+    sents, labels = read_line_examples_from_file(data_path)
+    """
+    if data_type == "train":
+        import math
+        length = len(sents)
+        half = math.floor(length / 4)
+        sents = sents[0: half]
+        labels = labels[0: half]
+    """
+    # the input is just the raw sentence
+    inputs = [s.copy() for s in sents]
+
+    task = 'asqp'
+    if task == 'aste':
+        targets = get_para_aste_targets(inputs, labels)
+    elif task == 'tasd':
+        targets = get_para_tasd_targets(inputs, labels)
+    elif task == 'asqp':
+        if data_type == "test":
+            targets = get_para_asqp_targets_test(inputs, labels)
+            print(len(inputs), len(targets))
+            return inputs, targets
+        else:
+            if data_type == "train":
+                new_inputs, targets = get_para_asqp_targets(inputs, labels, top_k)
+                #exit()
+            else:
+                targets = get_para_asqp_targets_test(inputs, labels)
+                return inputs, targets
+            print(len(inputs), len(new_inputs), len(targets))
+    else:
+        raise NotImplementedError
+
+    return new_inputs, targets
+
+
+class ABSADataset(Dataset):
+    def __init__(self, tokenizer, data_dir, data_type, top_k, max_len=128):
+        # './data/rest16/train.txt'
+        self.data_path = f'../data/{data_dir}/{data_type}.txt'
+        self.max_len = max_len
+        self.tokenizer = tokenizer
+        self.data_dir = data_dir
+        self.data_type = data_type
+
+        self.top_k = top_k
+
+        self.inputs = []
+        self.targets = []
+
+        self._build_examples()
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        source_ids = self.inputs[index]["input_ids"].squeeze()
+        target_ids = self.targets[index]["input_ids"].squeeze()
+
+        src_mask = self.inputs[index]["attention_mask"].squeeze()  # might need to squeeze
+        target_mask = self.targets[index]["attention_mask"].squeeze()  # might need to squeeze
+
+        return {"source_ids": source_ids, "source_mask": src_mask, 
+                "target_ids": target_ids, "target_mask": target_mask}
+
+    def _build_examples(self):
+
+        inputs, targets = get_transformed_io(self.data_path, self.data_dir, self.data_type, self.top_k)
+
+        for i in range(len(inputs)):
+            # change input and target to two strings
+            input = ' '.join(inputs[i])
+            target = targets[i]
+
+            tokenized_input = self.tokenizer.batch_encode_plus(
+              [input], max_length=self.max_len, padding="max_length",
+              truncation=True, return_tensors="pt"
+            )
+            tokenized_target = self.tokenizer.batch_encode_plus(
+              [target], max_length=self.max_len, padding="max_length",
+              truncation=True, return_tensors="pt"
+            )
+
+            self.inputs.append(tokenized_input)
+            self.targets.append(tokenized_target)
---
+++ DLO/eval_utils.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+
+# This script handles the decoding functions and performance measurement
+
+import re
+from data_utils import aspect_cate_list
+import numpy as np
+
+sentiment_word_list = ['positive', 'negative', 'neutral']
+opinion2word = {'great': 'positive', 'bad': 'negative', 'ok': 'neutral'}
+opinion2word_under_o2m = {'good': 'positive', 'great': 'positive', 'best': 'positive',
+                          'bad': 'negative', 'okay': 'neutral', 'ok': 'neutral', 'average': 'neutral'}
+numopinion2word = {'SP1': 'positive', 'SP2': 'negative', 'SP3': 'neutral'}
+
+
+def extract_spans_para(task, seq, seq_type):
+    quads = []
+    sents = [s.strip() for s in seq.split('[SSEP]')]
+    if task == 'aste':
+        for s in sents:
+            # It is bad because editing is problem.
+            try:
+                c, ab = s.split(' because ')
+                c = opinion2word.get(c[6:], 'nope')    # 'good' -> 'positive'
+                a, b = ab.split(' is ')
+            except ValueError:
+                # print(f'In {seq_type} seq, cannot decode: {s}')
+                a, b, c = '', '', ''
+            quads.append((a, b, c))
+    elif task == 'tasd':
+        for s in sents:
+            # food quality is bad because pizza is bad.
+            try:
+                ac_sp, at_sp = s.split(' because ')
+                
+                ac, sp = ac_sp.split(' is ')
+                at, sp2 = at_sp.split(' is ')
+                
+                sp = opinion2word.get(sp, 'nope')
+                sp2 = opinion2word.get(sp2, 'nope')
+                if sp != sp2:
+                    print(f'Sentiment polairty of AC({sp}) and AT({sp2}) is inconsistent!')
+                
+                # if the aspect term is implicit
+                if at.lower() == 'it':
+                    at = 'NULL'
+            except ValueError:
+                # print(f'In {seq_type} seq, cannot decode: {s}')
+                ac, at, sp = '', '', ''
+            
+            quads.append((ac, at, sp))
+    elif task == 'asqp':
+        for s in sents:
+            # food quality is bad because pizza is over cooked.
+            try:
+                index_ac = s.index("[AC]")
+                index_sp = s.index("[SP]")
+                index_at = s.index("[AT]")
+                index_ot = s.index("[OT]")
+
+                combined_list = [index_ac, index_sp, index_at, index_ot]
+                arg_index_list = list(np.argsort(combined_list))  # .tolist()
+
+                result = []
+                for i in range(len(combined_list)):
+                    start = combined_list[i] + 3
+                    sort_index = arg_index_list.index(i)
+                    if sort_index < 3:
+                        next_ = arg_index_list[sort_index + 1]
+                        re = s[start: combined_list[next_]]
+                    else:
+                        re = s[start:]
+                    result.append(re.strip())
+
+                ac, sp, at, ot = result
+
+                # if the aspect term is implicit
+                if at.lower() == 'it':
+                    at = 'NULL'
+            except ValueError:
+                try:
+                    # print(f'In {seq_type} seq, cannot decode: {s}')
+                    pass
+                except UnicodeEncodeError:
+                    # print(f'In {seq_type} seq, a string cannot be decoded')
+                    pass
+                ac, at, sp, ot = '', '', '', ''
+
+            quads.append((ac, at, sp, ot))
+    else:
+        raise NotImplementedError
+    return quads
+
+
+def compute_f1_scores(pred_pt, gold_pt):
+    """
+    Function to compute F1 scores with pred and gold quads
+    The input needs to be already processed
+    """
+    # number of true postive, gold standard, predictions
+    n_tp, n_gold, n_pred = 0, 0, 0
+
+    for i in range(len(pred_pt)):
+        n_gold += len(gold_pt[i])
+        n_pred += len(pred_pt[i])
+
+        for t in pred_pt[i]:
+            if t in gold_pt[i]:
+                n_tp += 1
+
+    print(f"number of gold spans: {n_gold}, predicted spans: {n_pred}, hit: {n_tp}")
+    precision = float(n_tp) / float(n_pred) if n_pred != 0 else 0
+    recall = float(n_tp) / float(n_gold) if n_gold != 0 else 0
+    f1 = 2 * precision * recall / (precision + recall) if precision != 0 or recall != 0 else 0
+    scores = {'precision': precision, 'recall': recall, 'f1': f1}
+
+    return scores
+
+
+def compute_scores(pred_seqs, gold_seqs, sents):
+    """
+    Compute model performance
+    """
+    assert len(pred_seqs) == len(gold_seqs)
+    num_samples = len(gold_seqs)
+
+    all_labels, all_preds = [], []
+
+    for i in range(num_samples):
+        gold_list = extract_spans_para('asqp', gold_seqs[i], 'gold')
+        pred_list = extract_spans_para('asqp', pred_seqs[i], 'pred')
+
+        print("gold ", gold_seqs[i])
+        print("pred ", pred_seqs[i])
+
+        all_labels.append(gold_list)
+        all_preds.append(pred_list)
+
+    #print("all_preds_labels")
+    #for i in range(len(all_preds)):
+    #    print(all_preds[i], all_labels[i])
+
+    print("\nResults:")
+    scores = compute_f1_scores(all_preds, all_labels)
+    print(scores)
+
+    return scores, all_labels, all_preds
